f97d35c44ed98c532ecbbdd86abeefec437989d1
fielderr1 <fielder@operamail.com>
2016-09-26 16:34:18 -0700

    Update Replication checks.sql

--- Replication checks.sql
+++ Replication checks.sql
@@ -5,13 +5,13 @@ go
 select is_published,is_subscribed,is_merge_published, is_distributor,is_cdc_enabled,is_broker_enabled, * from sys.databases
 
 --DB objects that are replicated
-SELECT tbl.ID,
-		tbl.Name,
-		tbl.ReplInfo
+SELECT	tbl.ID,
+	tbl.Name,
+	tbl.ReplInfo
 
 FROM sysobjects tbl
-	JOIN sysusers usr
-	  ON usr.uid = tbl.uid
+	JOIN sysusers usr ON usr.uid = tbl.uid
+	  
 WHERE usr.name = 'dbo' and
 	  tbl.xtype = 'U' and
 	  tbl.ReplInfo = 1
