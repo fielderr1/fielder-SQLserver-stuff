be1a61bff2e1c5a7addada8e0d12716ca637abbe
fielderr1 <fielder@operamail.com>
2016-09-26 14:23:43 -0700

    fixed type

--- Shrink TempDB.sql
+++ Shrink TempDB.sql
@@ -22,24 +22,24 @@ execute sp_helpdb tempdb;
 
 ----if log refuses to shrink
 --select log_reuse_wait_desc, is_cdc_enabled, is_broker_enabled, is_published,is_subscribed,is_merge_published, is_distributor, * from sys.databases
-----if replication this flush old transactions from queue backupset had camptured
+----if replication this flush old transactions from queue backupset had captured
 --execute sp_repldone @xact_sgno = Null, @numtrans = 0, @time = 0, @reset = 1
 
 use master;
 go
 
-alter database IGT_Wy
+alter database dbName
 set recovery simple;
 go
 
-use IBT_WY
+use dbName
 go
-dbcc shrinkfile (N'IGT_WY_log',0,TruncateOnly);
+dbcc shrinkfile (N'dbName_log',0,TruncateOnly);
 go
 
 use master
 go
-alter database IGT_Wy
+alter database dbName
 set recovery full;
 go
 
