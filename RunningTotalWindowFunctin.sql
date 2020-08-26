5b2987553e30a4311f2121fbabe0ce2219dc00e0
Rob <fielder@operamail.com>
2020-08-26 14:24:42 -0700

    Update RunningTotalWindowFunctin.sql

--- RunningTotalWindowFunctin.sql
+++ RunningTotalWindowFunctin.sql
@@ -1,3 +1,15 @@
+USE AdventureWorks2016;
+GO
+SELECT ROW_NUMBER() OVER(ORDER BY CustomerID, TotalDue DESC) AS RowNum, 
+       CustomerID, 
+       SalesOrderID, 
+       OrderDate, 
+       SalesOrderNumber, 
+       SubTotal, 
+       TotalDue
+FROM Sales.SalesOrderHeader
+
+
 -- Running Total for SQL Server 2012 and Later Version
 SELECT ID, Value,
 SUM(Value) OVER(ORDER BY ID ROWS UNBOUNDED PRECEDING) AS RunningTotal
